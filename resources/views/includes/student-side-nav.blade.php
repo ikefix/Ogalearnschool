a4fbd7ed52c75d2b16f8f9c7fcc36f67fee116fc
ikefix <omekeikechukwu877@gmail.com>
2025-07-28 17:19:43 +0100

    responsive work

--- resources/views/includes/student-side-nav.blade.php
+++ resources/views/includes/student-side-nav.blade.php
@@ -11,7 +11,7 @@
         </li>
         <li>
             <a href="{{ route('student.courses') }}" class="nav-link">
-                My Courses
+                My Courses1
             </a>
         </li>
         <li>
